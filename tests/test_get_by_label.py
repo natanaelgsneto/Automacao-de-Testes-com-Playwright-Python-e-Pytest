e94fecaa599741eb2139cda6b9ddf64fc90f94a2
natanaelgsneto <natanaelguedessn@gmail.com>
2026-07-28 12:41:27 -0300

    add
    add

--- tests/test_get_by_label.py
+++ tests/test_get_by_label.py
@@ -2,4 +2,5 @@ def test_get_by_label(page):
     page.goto("https://bootswatch.com/default/")
     page.pause()
     page.get_by_label('Valid input',exact=True).fill('Teste')
+    page.get_by_label("Recipient's username", exact=True)
     #no playwight inspector clicar em Step Over e fecha a janela do playwight
