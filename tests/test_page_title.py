b24aeb0a672e1900d91c2dbb20987634ac7ab0ab
natanaelgsneto <natanaelguedessn@gmail.com>
2026-07-28 21:12:42 -0300

    add
    add

--- tests/test_page_title.py
+++ tests/test_page_title.py
@@ -5,3 +5,8 @@ def test_google(page):
 
     assert "Google" in page.title() #para terminar o teste tem que fechar o Playwright Inspector
     #no playwight inspector clicar em Step Over e fecha a janela do playwight
+def test_title(page):
+    page.goto("https://bootswatch.com/default/")
+    page.pause()
+    page.get_by_title("Source Title").nth(0).click()
+
