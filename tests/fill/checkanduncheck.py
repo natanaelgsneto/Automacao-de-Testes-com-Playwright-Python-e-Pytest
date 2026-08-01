e10fd47072d976c7324596ba1e4708a03d490619
natanaelgsneto <natanaelguedessn@gmail.com>
2026-08-01 17:03:01 -0300

    add

--- tests/fill/checkanduncheck.py
+++ tests/fill/checkanduncheck.py
@@ -7,3 +7,23 @@ def test_checkbox_uncheck(page):
     page.get_by_role("checkbox", name="Default checkbox").uncheck()
     page.get_by_role("checkbox", name="Default checkbox").check()
     page.pause()
+
+def test_select_option(page):
+        page.goto("https://bootswatch.com/default/")
+
+        page.pause()
+
+        # Select normal
+        page.get_by_label("Example select").select_option("3")
+
+        # Select múltiplo
+        select = page.get_by_label("Example multiple select")
+
+        select.select_option(label=["1", "3"])
+
+        # Verificar opções selecionadas
+        print(
+            select.locator("option:checked").evaluate_all(
+                "(options) => options.map(o => o.textContent)"
+            )
+        )
