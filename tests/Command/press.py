f71c82dcda65e2b3140e9f3dc956534e43a27ab7
natanaelgsneto <natanaelguedessn@gmail.com>
2026-08-02 12:50:59 -0300

    add
    add

---
+++ tests/Command/press.py
@@ -0,0 +1,11 @@
+def test_press(page):
+    page.goto("https://bootswatch.com/default/")
+    page.pause()
+
+    page.get_by_placeholder("name@example.com").fill("teste@teste.com")
+    page.get_by_placeholder("name@example.com").press("Tab")
+
+    # Digita lentamente
+    page.keyboard.type("1234", delay=500)
+
+    page.wait_for_timeout(5000)
