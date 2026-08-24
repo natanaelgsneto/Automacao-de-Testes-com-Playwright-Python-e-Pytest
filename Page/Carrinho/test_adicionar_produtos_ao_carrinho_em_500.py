8f429a7b503c7c7b2c3be9812b73f2ef59e9c9d2
natanaelgsneto <natanaelguedessn@gmail.com>
2026-08-23 22:18:26 -0300

    add

--- Page/Carrinho/test_adicionar_produtos_ao_carrinho_em_500.py
+++ Page/Carrinho/test_adicionar_produtos_ao_carrinho_em_500.py
@@ -1,5 +1,4 @@
 from Page.Carrinho.Produto import Produtos
-from Page.Carrinho.Carrinho import Carrinho
 
 
 def test_adicionar_produtos_ao_carrinho(page):
@@ -7,11 +6,22 @@ def test_adicionar_produtos_ao_carrinho(page):
 
     produtos.acessar_produtos()
 
-    precoprodutos = produtos.card_produto.nth(1).locator('.productinfo h2').inner_text().replace('Rs.', '')
+    precoprodutos = (
+        produtos.card_produto
+        .nth(1)
+        .locator('.productinfo h2')
+        .inner_text()
+        .replace('Rs.', '')
+    )
+
+    precoprodutos = int(precoprodutos.strip())
 
     print(precoprodutos)
+    print(type(precoprodutos))
 
     page.pause()
 
-    #para rodar
-    #python -m pytest Page/Carrinho/test_adicionar_produtos_ao_carrinho_em_500.py::test_adicionar_produtos_ao_carrinho -v
+    if precoprodutos <= 500:
+        print("Produto custa até 500")
+    else:
+        print("Produto custa mais de 500")
